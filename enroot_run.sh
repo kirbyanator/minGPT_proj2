c7997b784abd8c8898263a87589c2315d0621250
Davis Forster <dp4ster007@me.com>
2023-11-12 05:50:55 +0000

    fixed cache crap

--- enroot_run.sh
+++ enroot_run.sh
@@ -3,6 +3,10 @@ module load jq zstd pigz parallel libnvidia-container enroot
 
 CONTAINER_NAME="mingpt"
 
+export HF_CACHE=/tmp
+export TRANSFORMERS_CACHE=/tmp
+export HF_DATASETS_CACHE=/tmp
+
 # Check if container already exists using enroot list
 if ! enroot list | grep -q "^${CONTAINER_NAME}\$"; then
     enroot create --force --name $CONTAINER_NAME ${HOME}/sqsh_files/mingpt.sqsh
