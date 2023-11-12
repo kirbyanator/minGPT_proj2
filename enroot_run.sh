33af43099fed29da5a332abc453865ae035dbefe
Davis Forster <dp4ster007@me.com>
2023-11-12 05:09:40 +0000

    fixed again

--- enroot_run.sh
+++ enroot_run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 module load jq zstd pigz parallel libnvidia-container enroot   
 
-CONTAINER_NAME="min_gpt"
+CONTAINER_NAME="mingpt"
 
 # Check if container already exists using enroot list
 if ! enroot list | grep -q "^${CONTAINER_NAME}\$"; then
-    enroot create --force --name $CONTAINER_NAME ${HOME}/sqsh_files/min_gpt.sqsh
+    enroot create --force --name $CONTAINER_NAME ${HOME}/sqsh_files/mingpt.sqsh
 fi                
 
 #    --mount ${HOME}/hf_models:/app/hf_models \
 
 enroot start \
        --mount .:/app \
-       min_gpt \
+       mingpt \
        bash # the name of the command INSIDE THE CONTAINER that you want to run
